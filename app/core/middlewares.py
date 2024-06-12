4fa874d58bb34c86eb4806e183046d2183c8b08c
André Lopes <andrelopes.code@gmail.com>
2024-06-12 08:30:24 -0300

    ✨ feat: add cors middleware to app

---
+++ app/core/middlewares.py
@@ -0,0 +1,25 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+
+# This function is used to configure
+# CORS middleware for the FastAPI app
+def configure_cors(app):
+    origins = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+    ]
+
+    methods = ["*"]
+    headers = ["*"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=methods,
+        allow_headers=headers,
+    )
